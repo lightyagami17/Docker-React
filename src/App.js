e91cefd57cab9661e8f1150a5f9f9681072dc007
lightyagami17 <gayatrisai1080@gmail.com>
2023-01-02 15:19:28 +0530

    squashed bugs

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Just changed
+        <p>  learn react</p>
         </a>
       </header>
     </div>
